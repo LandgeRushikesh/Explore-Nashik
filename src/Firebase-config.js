e635d39b528a9414cfca5a10756fd136b4589783
LandgeRushikesh <rushilandge12@gmail.com>
2025-08-10 15:38:04 +0530

    Firebase configured

---
+++ src/Firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: String(import.meta.env.VITE_FIREBASE_APIKEY),
+  authDomain: String(import.meta.env.VITE_FIREBASE_AUTHDOMAIN),
+  projectId: String(import.meta.env.VITE_FIREBASE_PROJECTID),
+  storageBucket: String(import.meta.env.VITE_FIREBASE_STORAGEBUCKET),
+  messagingSenderId: String(import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID),
+  appId: String(import.meta.env.VITE_FIREBASE_APPID),
+  measurementId: String(import.meta.env.VITE_FIREBASE_MEASUREMENTID)
+}
+
+const app = initializeApp(firebaseConfig);
