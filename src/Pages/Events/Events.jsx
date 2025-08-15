3a082d1b7430bfbeaf96221c14478bc2656cd678
LandgeRushikesh <rushilandge12@gmail.com>
2025-08-15 11:57:43 +0530

    Refactor Events component to use Card for event display

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -1,6 +1,7 @@
 import { collection, getDocs } from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
 import { DataContext } from "../../Context/DataContext";
+import Card from "../../Components/Card/Card";
 
 function Events() {
 
@@ -9,15 +10,7 @@ function Events() {
   return (
     <div className="event-cards w-full mt-28 min-h-[80vh] grid grid-cols-4 px-4">
       {AllEvents.map((event) => (
-        <div key={event.id} className="w-[350px] shadow-lg shadow-gray-800 my-5 mx-2 py-4 px-3 flex shrink-0 flex-col justify-center items-center hover:scale-[1.02] hover:shadow-xl hover:shadow-gray-600 transition-all duration-300">
-          <img
-            src={event.imgURL}
-            alt="place image"
-            className="w-full h-48 object-cover"
-          />
-          <h3 className="text-lg text-gray-800 font-bold">{event.Name}</h3>
-          <p className="text-sm text-gray-600">{event.shortDesc}</p>
-        </div>
+        <Card attraction={event} />
       ))}
     </div>
   );
