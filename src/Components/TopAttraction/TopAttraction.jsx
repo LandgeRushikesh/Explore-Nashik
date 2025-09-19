79cc9c11b523699dfa539452f0bb87a5a8485212
LandgeRushikesh <rushilandge12@gmail.com>
2025-09-19 23:22:11 +0530

    Refactor TopAttraction and TopEvents components to utilize context for data fetching; remove unused Firebase calls and improve state management

--- src/Components/TopAttraction/TopAttraction.jsx
+++ src/Components/TopAttraction/TopAttraction.jsx
@@ -1,26 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
 import { db } from "../../Firebase-config";
 import Card from "../Card/Card";
+import { DataContext } from "../../Context/DataContext";
 
 function TopAttraction() {
   const [attractions, setAttractions] = useState([]);
 
-  const collectionRef = collection(db, "Top Attraction");
+  const { places } = useContext(DataContext);
 
   useEffect(() => {
-    const fetchPlaces = async () => {
-      try {
-        const res = await getDocs(collectionRef);
-        setAttractions(res.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-      } catch (err) {
-        alert(err.message);
-      }
-    };
-
-    fetchPlaces();
-  }, []);
+    setAttractions(places.filter((place) => place.showToHome === true));
+  }, [places]);
 
   return (
     <div className="attraction-cards w-full flex justify-start items-center gap-4 overflow-x-auto px-6 font-serif">
--- src/Components/TopEvents/TopEvents.jsx
+++ src/Components/TopEvents/TopEvents.jsx
@@ -1,21 +1,17 @@
 import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { db } from "../../Firebase-config";
 import Card from "../Card/Card";
+import { DataContext } from "../../Context/DataContext";
 
 function TopEvents() {
   const [events, setEvents] = useState([]);
 
-  const collectionRef = collection(db, "Top Events");
+  const { AllEvents } = useContext(DataContext);
 
   useEffect(() => {
-    const fetchEvents = async () => {
-      const res = await getDocs(collectionRef);
-      setEvents(res.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-    };
-
-    fetchEvents();
-  }, []);
+    setEvents(AllEvents.filter((event) => event.showToHome === true));
+  }, [AllEvents]);
 
   return (
     <div className="event-cards w-full flex justify-start items-center gap-4 overflow-x-auto px-4">
